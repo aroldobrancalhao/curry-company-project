4091cfc34fca21a1bca9aa3e10634ec4847baaf2
aroldobrancalhao <133703463+aroldobrancalhao@users.noreply.github.com>
2023-06-10 22:40:05 -0300

    Update 2_visao_entregadores.py

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -152,7 +152,7 @@ tab1, tab2, tab3  =st.tabs(['Visão Gerencial', '_', '_'])
 
 with tab1:
     with st.container():
-        st.header('Métricas Gerias')
+        st.header('Métricas Gerais')
 
         col1, col2, col3, col4 = st.columns(4, gap='large')
         with col1:
